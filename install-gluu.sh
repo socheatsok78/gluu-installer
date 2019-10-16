b03aaf6113598b06861732859960cf575f659722
Socheat <socheatsok78@gmail.com>
2019-10-16 17:47:16 +0700

    Update install-gluu.sh, Add more detail to echo

--- install-gluu.sh
+++ install-gluu.sh
@@ -23,7 +23,7 @@ case "$LSB_RELEASE_CODENAME" in
         LINUX_CODENAME="stretch-stable"
     ;;
     *)
-        LINUX_CODENAME=${LSB_RELEASE_CODENAME:-xenial}
+        LINUX_CODENAME=${LSB_RELEASE_CODENAME}
     ;;
 esac
 
@@ -33,17 +33,21 @@ echo "Codename: $LINUX_CODENAME"
 echo "--------------------------------------------------"
 
 # Add Gluu Repository
-echo "Add Gluu Repository"
-echo "deb https://repo.gluu.org/${LINUX_DISTRO}/ ${LINUX_CODENAME} main" > /etc/apt/sources.list.d/gluu-repo.list
+APT_REPO="deb https://repo.gluu.org/${LINUX_DISTRO}/ ${LINUX_CODENAME} main"
+
+echo "Add Gluu Repository: $APT_REPO"
+echo $APT_REPO > /etc/apt/sources.list.d/gluu-repo.list
 
 # Add Gluu GPG Key
-echo "Add Gluu GPG Key"
-curl https://repo.gluu.org/${LINUX_DISTRO}/gluu-apt.key | apt-key add -
+GPG_KEY="https://repo.gluu.org/${LINUX_DISTRO}/gluu-apt.key"
+
+echo "Add Gluu GPG Key: $GPG_KEY"
+curl "$GPG_KEY" | apt-key add -
 
 # Update/Clean Repo
 echo "Update/Clean Repo"
 apt-get update
 
 # Install Gluu Server
-echo "Install Gluu Server"
+echo "Install Gluu Server: gluu-server-${GLUU_VERSION}"
 apt-get install "gluu-server-${GLUU_VERSION}"
