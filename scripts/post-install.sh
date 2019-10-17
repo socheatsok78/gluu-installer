9b2385d4df7dcaeef55ca07a873c505ccb9a4907
Socheat <socheatsok78@gmail.com>
2019-10-17 09:56:05 +0700

    Print setup and error log on post-install.sh

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -6,3 +6,9 @@ source ./variables.sh
 
 echo ">>> Login to the gluu-server vm..."
 service gluu-server-${GLUU_VERSION} login < deploy.sh
+
+echo ">>> Setup log"
+cat /install/community-edition-setup/setup.log
+
+echo ">>> Error log"
+cat /install/community-edition-setup/setup_error.log
