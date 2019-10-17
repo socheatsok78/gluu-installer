9a6e83432178d30a331c96c29517a6b77b81c092
Socheat <socheatsok78@gmail.com>
2019-10-17 10:41:08 +0700

    Add setup.sh, auto installer

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+GLUU_INSTALLER_REPO="https://github.com/socheatsok78/gluu-installer"
+GLUU_INSTALLER_DIR="gluu-installer"
+
+function clone_installer_repo() {
+    echo ">>> Cloning $GLUU_INSTALLER_REPO to $GLUU_INSTALLER_DIR folder"
+    git clone "$GLUU_INSTALLER_REPO" "$GLUU_INSTALLER_DIR"
+}
+
+function gluu_setup() {
+    # Check if directory exists
+    if ! [ -d "$GLUU_INSTALLER_DIR" ]; then
+        exit 1;  
+    fi
+    
+    cd "$GLUU_INSTALLER_DIR"
+
+    echo ">>> Checking executable"
+    chmod +x ./install-gluu.sh
+    chmod +x ./deploy.sh
+    chmod +x ./scripts/post-install.sh
+
+    echo ">>> Running scripts"
+    ./install-gluu.sh
+}
+
+function main() {
+    clone_installer_repo
+    
+    gluu_setup
+}
+
+# Running main()
+main
