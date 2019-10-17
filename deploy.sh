3090b6c3bf769f7543e508d2b1981ec4d2fae32e
Socheat <socheatsok78@gmail.com>
2019-10-17 10:10:14 +0700

    Fix post-install.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,26 @@
 
 set -e
 
+GLUU_WORKDIR="/install/community-edition-setup"
+
 # Accept MIT license
 echo 'yes'
 
-cd /install/community-edition-setup
+echo ">>> Changing working directory to: $GLUU_WORKDIR"
+cd "$GLUU_WORKDIR"
+
+echo ">>> Starting setup.py script"
 ./setup.py
 
-exit 0;
+# Print setup.log
+if [ -f "$GLUU_WORKDIR/setup.log" ]; then
+    echo ">>> Setup log"
+    cat "$GLUU_WORKDIR/setup.log"
+fi
+
+# Print setup_error.log
+if [ -f "$GLUU_WORKDIR/setup_error.log" ]; then
+    echo ">>> Error log"
+    cat "$GLUU_WORKDIR/setup_error.log"
+fi
+
--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -6,9 +6,3 @@ source ./variables.sh
 
 echo ">>> Login to the gluu-server vm..."
 service gluu-server-${GLUU_VERSION} login < deploy.sh
-
-echo ">>> Setup log"
-cat /install/community-edition-setup/setup.log
-
-echo ">>> Error log"
-cat /install/community-edition-setup/setup_error.log
